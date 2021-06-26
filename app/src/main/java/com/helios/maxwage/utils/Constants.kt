4e791bfea341dd250839f847b895c6b4302d8aba
pduy99 <1712018@student.hcmus.edu.vn>
2021-06-26 23:49:00 +0700

    [i] Inject the host IP with Gradle

--- app/src/main/java/com/helios/maxwage/utils/Constants.kt
+++ app/src/main/java/com/helios/maxwage/utils/Constants.kt
@@ -7,8 +7,6 @@ package com.helios.maxwage.utils
 object Constants {
     const val SHARED_PREFERENCES_NAME = "com.helios.maxwage.shared_prefs"
 
-    const val SERVER_HOST = "http://34.126.68.152:8080/v1/"  //change the IP here
-
     const val MAX_WORKING_TIME = 32
 }
 
